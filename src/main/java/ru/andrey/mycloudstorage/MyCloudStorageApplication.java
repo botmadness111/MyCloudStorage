3d6e00229c7560853d424450ba1379f84221dbbd
qqandrey <pap_pap_1414@mail.ru>
2024-05-01 13:27:05 +0300

    initial commit

---
+++ src/main/java/ru/andrey/mycloudstorage/MyCloudStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.andrey.mycloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCloudStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCloudStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andrey/mycloudstorage/MyCloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andrey.mycloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
